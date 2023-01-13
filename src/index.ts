06deb06854fc8cb60b64beebd32c7224d24a241a
akp07 <120704119+akp07@users.noreply.github.com>
2023-01-14 05:25:18 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+//1
+function PRODUCT(x: number, y: number): number {
+  return x * y;
+}
+
+console.log("PRODUCT : " + PRODUCT(1, 2));
+
+//2
+function DIV(x: number, y: number): number {
+  return x / y;
+}
+
+console.log("DIVIDING : " + DIV(10, 2));
+
+//3
+function name(): void {
+  console.log("AKASH KUMAR PATHAK");
+}
+
+name();
